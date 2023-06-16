8a83ea8d3180fe24f35b160e5b27cde5531dbfba
Sargam <sargampoudel100@gmail.com>
2023-06-16 11:40:05 +0530

    :art: fix: implement guard clause

--- src/middleware/jwtAuth.js
+++ src/middleware/jwtAuth.js
@@ -2,18 +2,17 @@ import jwt from 'jsonwebtoken';
 
 export const jwtAuth = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  console.log(token);
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        res.json({ error: err.message });
-      } else {
-        console.log(decodedToken);
-        next();
-      }
-    });
-  } else {
+  // Guard clause
+  if (!token) {
     res.status(401).json({ error: 'Unauthorized' });
   }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      res.json({ error: err.message });
+    } else {
+      console.log(decodedToken);
+      next();
+    }
+  });
 };
