5db246cf072695aaf4b04699bea670c94e77b3d0
Sargam <sargampoudel100@gmail.com>
2023-09-27 01:43:06 +0530

    :green_heart: feat: setup husky precommit hook

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const dbConnect = async () => {
   try {
     if (!process.env.MONGO_URL)
-              throw new Error('Missing MONGO_URL env variable');
+      throw new Error('Missing MONGO_URL env variable');
     const connection = await mongoose.connect(process.env.MONGO_URL);
     console.log(`Connected to MongoDB ${connection.connection.host}`);
   } catch (error) {
-           console.log(error);
-  } 
+    console.log(error);
+  }
 };
 
 export default dbConnect;
